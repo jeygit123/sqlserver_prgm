36988847dd39d27cd5c736ce55ac29ea87b77665
jeygit123 <erjey.se@gmail.com>
2022-04-18 18:49:12 -0400

    commited

---
+++ jdbc-demo/src/main/java/com/training/jdbd/jdbcTest.java
@@ -0,0 +1,24 @@
+package com.training.jdbd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class jdbcTest {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException 
+	   {
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		String url ="jdbc:sqlserver://localhost:1433;"+"databaseName=Mars;"+"encrypt=true;trustServerCertificate = true";
+		String user = "sa";
+		String password="Hotwater007";
+		Connection con = DriverManager.getConnection(url, user, password);
+		
+		if (con != null) {
+			System.out.println("Connection Established!!");
+		}else {
+			System.out.println("Connection Refused!!");
+		}
+		}
+
+}
